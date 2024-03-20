337566e1ee209dd365a38d4a94dfb15618fa437d
johanyim <37012949+johanyim@users.noreply.github.com>
2024-03-20 01:40:05 +0000

    a lot of optimizations

--- assets/interaction-D9n5lnV4.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/Linker-CktrOvzo.js","assets/OptionsColor-CMqUFD6Z.js","assets/index-b2LIg75-.js","assets/index-RRWjUmfX.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as i}from"./index-b2LIg75-.js";async function o(t,r=!0){await t.addInteractor("particlesLinks",async a=>{const{Linker:n}=await i(()=>import("./Linker-CktrOvzo.js"),__vite__mapDeps([0,1,2,3]));return new n(a)},r)}export{o as loadLinksInteraction};
