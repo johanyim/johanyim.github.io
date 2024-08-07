6584404d426c80ad34640c1e8be678feee7b4196
johanyim <37012949+johanyim@users.noreply.github.com>
2024-08-07 23:28:43 +0100

    REMOVED: using base in urls for blog

--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -1,5 +1,5 @@
 import type { SvelteComponent } from "svelte";
-import { base } from "$app/paths";
+// import { base } from "$app/paths";
 
 
 
@@ -26,11 +26,14 @@ export function load() {
     const glob_import = import.meta.glob<Glob>('$posts/*.md', { eager: true });
     const paths: [string, Glob][] = Object.entries(glob_import);
 
+    let ps = paths.map((p) => p[0].split('/').pop()?.replace('.md', '') || '');
+
+    console.log(JSON.stringify(ps, null, 2))
 
     return {
         posts: paths.map((p) => ({
             data: p[1].metadata,
-            url: `${base}/blog/` + p[0].split('/').pop()?.replace('.md', '') || '',
+            url: `blog/` + p[0].split('/').pop()?.replace('.md', '') || '',
 
         })).sort((first, second) =>
             new Date(second.data.date).getTime() - new Date(first.data.date).getTime()
