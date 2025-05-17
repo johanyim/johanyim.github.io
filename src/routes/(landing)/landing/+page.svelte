832b3c2c94105e0c5bc32c25154855a08fe2803a
johanyim <johanjyyim@gmail.com>
2025-05-17 16:28:33 +0100

    search styling

--- src/routes/(landing)/landing/+page.svelte
+++ src/routes/(landing)/landing/+page.svelte
@@ -1,18 +1,38 @@
 <script>
+    import Magnify from "~icons/mdi/magnify";
+    import {onMount, tick} from 'svelte' 
+    import Line from "$atoms/Line.svelte";
 
+    let ref = null
+    onMount(async () => {
+        await tick();
+        await ref.focus();
+    })
 </script>
 
+<div class="h-screen w-screen 
+    flex flex-col
+    p-4
+
+    ">
+
+
+    <form method="get" action="https://www.google.com/search"  class="flex text-4xl items-center ">
+        <Magnify class="flex-0"/>
+        <input type="text" name="q"
+            class="
+            outline-none
+            flex-1
+            bg-crust text-text
+            w-full
+            "  
+            bind:this={ref}
+        >
+            <!-- border-2 border-lavender rounded-lg  -->
+    </form>
+
+    <Line style="mb-16 mt-2 mx-2" color="#cdd6f4" />
 
 
 
-<div 
-
->
-
-
-<form method="get" action="https://www.google.com/search">
-  <input type="text" class="bg-crust text-text" name="q" placeholder="Search Google">
-  <input type="submit" value="Search">
-</form>
 </div>
-
