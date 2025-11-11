7b336591b52b42980e6fa7d762d26f7517c9f56d
johanyim <johanjyyim@gmail.com>
2025-11-11 21:07:23 +0000

    whatsapp goes to HK number

--- src/lib/components/molecules/SocialButtons.svelte
+++ src/lib/components/molecules/SocialButtons.svelte
@@ -7,10 +7,10 @@
   import Send from "~icons/mdi/send";
 </script>
 
-{#snippet social(href: string, aria: string, color: string, Icon: any) }
+{#snippet social(href: string,  aria: string, color: string, Icon: any, target?: string,) }
     <a
         {href}
-        target="_blank"
+        {target}
         aria-label={aria}
     >
         <Icon class="rounded-md {color} hover:bg-surface0 hover:text-text" />
@@ -19,19 +19,29 @@
 
 {@render social("https://github.com/johanyim",
     "Visit my GitHub profile", 
-    "text-mauve", Github)}
+    "text-mauve", Github,
+    "_blank"
+)}
 {@render social("https://www.linkedin.com/in/johanjyyim", 
     "Visit my LinkedIn profile", 
-    "text-sapphire", LinkedIn)}
+    "text-sapphire", LinkedIn,
+    "_blank"
+)}
 {@render social("mailto:johanjyyim@gmail.com", 
     "Open your mail client and send me a message", 
-    "text-red", Email)}
+    "text-red", Email,
+    "_blank"
+)}
 {@render social("https://github.com/johanyim.atom", 
     "View my RSS feed", 
-    "text-peach", Rss)}
-{@render social("https://wa.me/+447510787854", 
+    "text-peach", Rss,
+    "_blank"
+)}
+{@render social("https://wa.me/+85261897773", 
     "Message me on my WhatsApp", 
-    "text-green", WhatsApp)}
+    "text-green", WhatsApp,
+    "_blank"
+)}
 {@render social("#contact", 
     "Send me an Email", 
     "text-lavender", Send)}
